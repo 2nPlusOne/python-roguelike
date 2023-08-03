bbe75d3d785f001af80562a02967da244d91894b
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-08-03 14:14:54 -0400

    Finish part 5

--- game_map.py
+++ game_map.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Iterable, TYPE_CHECKING
+from typing import Iterable, Optional, TYPE_CHECKING
 
 import numpy as np
 from tcod.console import Console
@@ -19,6 +19,13 @@ class GameMap:
     self.visible = np.full((width, height), fill_value=False, order="F")
     self.explored = np.full((width, height), fill_value=False, order="F")
     
+  def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+    for entity in self.entities:
+      if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+        return entity
+    
+    return None
+    
   def in_bounds(self, x: int, y: int) -> bool:
     """Return True if x and y are inside of the bounds of this map."""
     return 0 <= x < self.width and 0 <= y < self.height
@@ -31,7 +38,7 @@ class GameMap:
     If it isn't, but it's in the "explored" array, then draw it with the "dark" colors.
     Otherwise, the default is "SHROUD".
     """
-    console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+    console.rgb[0:self.width, 0:self.height] = np.select(
       condlist=[self.visible, self.explored],
       choicelist=[self.tiles["light"], self.tiles["dark"]],
       default=tile_types.SHROUD

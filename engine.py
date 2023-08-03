18a20cf6d1e6259a093610ba63aed983b2595fbc
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-08-03 11:20:25 -0400

    Implemented fov

--- engine.py
+++ engine.py
@@ -2,8 +2,8 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
-from actions import EscapeAction, MovementAction
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
@@ -14,20 +14,35 @@ class Engine:
     self.event_handler = event_handler
     self.player = player
     self.game_map = game_map
+    self.fov_radius = 8
+    
+    self.update_fov() # Initialize the FOV
     
   def handle_events(self, events: Iterable[Any]) -> None:
     for event in events:
-      action = self.event_handler.dispatch(event)
-                
+      action = self.event_handler.dispatch(event) 
       if action is None:
         continue
       
       action.perform(self, self.player)
+      self.update_fov() # Update the FOV before the players next action
+      
+  def update_fov(self) -> None:
+    """Recompute the visible area based on the players point of view."""
+    self.game_map.visible = compute_fov(
+      self.game_map.tiles["transparent"],
+      (self.player.x, self.player.y),
+      radius=self.fov_radius,
+    )
+    # If a tile is "visible" it should be added to "explored"
+    self.game_map.explored |= self.game_map.visible
         
   def render(self, console: Console, context: Context) -> None:
     self.game_map.render(console)
     
     for entity in self.entities:
+      # Only print entities that are in the FOV
+      if not self.game_map.visible[entity.x, entity.y]: continue
       console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
       
     context.present(console)

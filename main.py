4c970dea3ad714649dbeed3b62c8db746cc6efb6
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-08-03 10:58:03 -0400

    finished dungeon generator

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
 from entity import Entity
 from input_handlers import EventHandler
 from engine import Engine
@@ -13,6 +11,10 @@ def main() -> None:
     
     map_width = 80
     map_height = 45
+    
+    room_max_size = 12
+    room_min_size = 6
+    max_rooms = 30
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -24,7 +26,14 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
     
-    game_map = generate_dungeon(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
     engine = Engine(entities=entities, event_handler=event_handler, player=player, game_map=game_map)
 
     with tcod.context.new_terminal(

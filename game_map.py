c082ec0046d23eb40cf4d14b2e3981bfe033bc89
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-08-08 17:20:46 -0400

    Added fighting mechanic and fixed render order

--- game_map.py
+++ game_map.py
@@ -74,7 +74,11 @@ class GameMap:
             default=tile_types.SHROUD,
         )
 
-        for entity in self.entities:
+        entities_sorted_for_rendering = sorted(
+            self.entities, key=lambda x: x.render_order.value
+        )
+
+        for entity in entities_sorted_for_rendering:
             # Only print entities that are in the FOV
             if not self.visible[entity.x, entity.y]:
                 continue
